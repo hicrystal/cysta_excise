5e8769e57635ff7f385f3027833e9912a0c3cbc3
Jing Gao <jing.gao@farfetch.com>
2021-11-30 20:08:48 +0800

    feat: add lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+    LessonModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Lesson
+      ],
+
+    }),
+    GraphQLModule.forRoot({
     autoSchemaFile: true
-  }), 
-  LessonModule],
+  }), ],
 
 })
 export class AppModule {}
--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,11 +1,21 @@
 
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Mutation } from "@nestjs/graphql";
 import { LessonType } from "./lesson.type";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
-  @Query()
+  @Query(returns => LessonType)
   lesson() {
+    return {
+      id: 'sdfasdf',
+      name: 'James',
+      startDate: (new Date().toISOString()),
+      endDate: (new Date().toISOString()),
+    }
+
+  }
+  @Mutation(returns => LessonType)
+  createLesson() {
 
   }
 }
