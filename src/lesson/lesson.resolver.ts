07c194ce0dd5d6a72f9450c7072c5a13d5408534
Jing Gao <jing.gao@farfetch.com>
2021-11-30 19:23:03 +0800

    feat: add module lesson

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,11 @@
+
+import { Resolver, Query } from "@nestjs/graphql";
+import { LessonType } from "./lesson.type";
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+  @Query()
+  lesson() {
+
+  }
+}
